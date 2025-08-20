3107a7b0e347f2c0885b7ee1d75d08613322aa51
santhos-dev <santhos.webdev@gmail.com>
2025-08-20 17:03:35 +0000

    added

--- comparisons/comparison.js
+++ comparisons/comparison.js
@@ -1,5 +1,6 @@
 console.log("Comparisons");
 // ---------------------------------------------------------------
+// greater than
 
 let a = 100;
 
@@ -8,3 +9,37 @@ if (a > 25){
 }else{
     console.log("Wrong");
 }
+
+// ----------------------
+// less than
+
+let b = 60;
+
+if (b < 50) {
+    console.log("Correct");
+} else{
+    console.log("Wrong");
+}
+
+// ---------------------
+// greater than or equal
+let c = 50;
+
+if (c >= 40) {
+    console.log("Correct");
+} else {
+    console.log("Wrong");
+}
+
+// ---------------------
+// less thsn or equal
+let d = 100;
+
+if (d <= 99) {
+    console.log("Correct");
+} else {
+    console.log("Wrong");
+}
+
+// ----------------------------------------------------------------------
+
